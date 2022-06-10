7143f29f7326abe33d4a4f4163f812348bba9b90
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-10 14:20:00 +0300

    Add books component

---
+++ src/components/Books.js
@@ -0,0 +1,44 @@
+// eslint-disable
+import React from 'react';
+import AddNewBook from './AddNewBook';
+import Book from './Book';
+
+const Books = () => {
+  const booksStore = [
+    {
+      id: 1,
+      topic: 'Action',
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: 2,
+      topic: 'Science Fiction',
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: 3,
+      topic: 'Economy',
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ];
+
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      {booksStore.map((book) => (
+        <div key={book.id} className="book-details">
+          <Book topic={book.topic} title={book.title} author={book.author} />
+        </div>
+      ))}
+      ;
+      <AddNewBook onSubmit={handleSubmit} />
+    </div>
+
+  );
+};
+
+export default Books;
