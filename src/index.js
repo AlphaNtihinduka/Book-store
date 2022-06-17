1c4b0270a46770a359a14bb11300744d2b7f99dc
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-17 23:16:16 +0300

    Modify index.js file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { fetchBook } from './Redux/Books/books';
 import store from './Redux/configureStore';
 import './index.css';
 import App from './App';
+
+store.dispatch(fetchBook());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
